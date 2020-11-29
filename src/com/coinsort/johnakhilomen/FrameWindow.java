5ff8e9fb7439377bd85a03e58e7e5852d4346c06
John Akhilomen <jonbasanti@gmail.com>
2020-11-29 15:33:37 -0600

    Added comments

--- src/com/coinsort/johnakhilomen/FrameWindow.java
+++ src/com/coinsort/johnakhilomen/FrameWindow.java
@@ -10,12 +10,18 @@ import javax.swing.JPanel;
 
 public class FrameWindow extends JFrame
 {
+	//A class to create a frame window when needed 
 	public FrameWindow(String title, JPanel panel)
 	{
+		//set window's default size 
 		 setSize(650, 180);  
-	     setLocationRelativeTo(null);  
+		 //place window on the center of the screen
+	     setLocationRelativeTo(null);
+	     //exit application when window frame closes
 	     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+	     //title
 	     this.setTitle(title);
+	     //add panel to frame
 		 this.add(panel);
 	}
 	
