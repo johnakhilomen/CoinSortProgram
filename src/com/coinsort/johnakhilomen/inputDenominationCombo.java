746c71e45fa4e2189b3e4a8b1945bbb32d40c385
John Akhilomen <jonbasanti@gmail.com>
2020-11-29 15:30:57 -0600

    Added comments

--- src/com/coinsort/johnakhilomen/inputDenominationCombo.java
+++ src/com/coinsort/johnakhilomen/inputDenominationCombo.java
@@ -4,6 +4,7 @@ import javax.swing.JComboBox;
 
 public class InputDenominationCombo{
 
+	//A class to create a combo box when needed
 	public JComboBox getJComoBox(String[] denominations) {
 		JComboBox inputDenomination = new JComboBox(denominations); 
 		inputDenomination.setFont(MainPanel.font);
--- src/com/coinsort/johnakhilomen/testCoinSorter.java
+++ src/com/coinsort/johnakhilomen/testCoinSorter.java
@@ -14,23 +14,33 @@ import javax.swing.JPanel;
 
 public class TestCoinSorter {
 
+	//main panel for the main window
 	public static MainPanel mainPanel; 
+	//main frame for main window
 	public static JFrame mainDialog;
+	//a static object for instantiating coin sorter
 	public static CoinSorter coinSorter; 
+	//default minimum value set to 0
 	public static int minValue = 0;
+	//default maximum value set to 10000
 	public static int maxValue = 10000;
 	public static ArrayList<String> coinList;
+	//default coin list for multiple coin calculator
 	public static  String[] inputDenominationsMultiplecoins = new String[] {"P200","P100", "P50", "P30","P20","P10"};
+	//default coin list for coin calculator
 	public static  String[] inputDenominations = new String[] {"£2", "£1", "P200","P100", "P50", "P30","P20","P10"};
 	public static void main(String[] args) {
+		//default menus  
 		String[] buttonNames = new String[] {"Coin calculator", "Multiple coin calculator", "Print coin list", "Set details",
 				"Display program configurations", "Quit the program"};
 		coinList = new ArrayList<String>(Arrays.asList(inputDenominations));
 		coinSorter = new CoinSorter("£", 0, 10000, coinList);
+		//Lets create main panel with buttons
 		mainPanel = new MainPanel(buttonNames);
+		//Lets add main panel to the main frame or main window
 		mainDialog = new FrameWindow("Coin sorting program", mainPanel);
+		//show main frame or main window when the program starts
 		mainDialog.setVisible(true);
-		System.out.print(Math.floor(9.1));
 	}
 	
 }
